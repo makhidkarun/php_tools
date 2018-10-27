ee17fcfa1c90904ef6f0cc1f1c39a3b177abe012
Leam Hall <leamhall@gmail.com>
2018-10-27 12:39:27 -0400

    Moved the generation methods to the character class.

--- chargen.php
+++ chargen.php
@@ -1,63 +1,7 @@
 <?php
 
-const STR = 0;
-const EDU = 4;
-
-function go_to_school(array $upp) {
-  $upp[EDU] += 3;
-  return $upp;
-}
-
-function go_to_the_gym(array $upp) {
-  $upp[STR] +=2;
-  return $upp;
-}
-
-function roll2() {
-  return random_int(1,6) + random_int(1,6);
-}
-
-function gen_upp() { 
-  for ($i = 1; $i <=6; $i++) {
-    $upp[] = roll2();
-  }
-  return $upp;
-}
-
-function get_name(string $gender) {
-  if ($gender == "M") {
-    $first_name_table = 'humaniti_male_first';
-  } else {
-    $first_name_table = 'humaniti_female_first';
-  }
-  $name = get_rand_data($first_name_table) . " ";
-
-  $last_name_table = 'humaniti_last';
-  $name .= get_rand_data($last_name_table);
-  return $name;
-}
-
-function get_rand_data(string $table) {
-  try {
-    $dbh = new PDO("sqlite:data/names.db");
-  } catch(PDOException $e) {
-    echo $e->getMessage();
-    exit;
-  }
-  $sql    = ("SELECT * from $table ORDER BY RANDOM() LIMIT 1;");
-  $stmt   = $dbh->query($sql);
-  $result = $stmt->fetch();
-  return  $result['name'];
-}
-
-$upp      = gen_upp();
-$gender   = random_int(0,1) ? "F" : "M";
-$name     = get_name($gender);
-
 require 'lib/character.php';
-$data['name']   = $name;
-$data['upp']    = $upp;
-$data['gender'] = $gender;
+$data['upp']    = [ 3,4,5,6,7,8];
 $person = new Character($data);
 $person->to_s();
 ?>
--- lib/character.php
+++ lib/character.php
@@ -2,8 +2,16 @@
 
 class Character {
   public function __construct($data) {
-    $this->name   = $data['name']; 
-    $this->gender = $data['gender'];
+    if (isset($data['gender'])) {
+      $this->gender = $data['gender'];
+    } else {
+      $this->gender = random_int(0,1) ? "F" : "M";
+    } 
+    if (isset($data['name'])) {
+      $this->name   = $data['name']; 
+    } else {
+      $this->name   = $this->get_name($this->gender);
+    }  
     $this->upp    = $data['upp'];
   }
 
@@ -12,14 +20,63 @@ class Character {
     print("$this->name [$this->gender] $upp_s \n");
   }
 
-  public function upp_to_s(array $upp) {
+  private function upp_to_s(array $upp) {
     $string = ''; 
     foreach ($upp as $s) {
       $string .= dechex($s);
     }
     return strtoupper($string);
   }
-}
 
+  private function roll2() {
+    return random_int(1,6) + random_int(1,6);
+  }
+
+  private function gen_upp() {
+    for ($i = 1; $i <=6; $i++) {
+      $upp[] = $this->roll2();
+    }
+    return $upp;
+  }
+
+  private function get_name(string $gender) {
+    if ($gender == "M") {
+      $first_name_table = 'humaniti_male_first';
+    } else {
+      $first_name_table = 'humaniti_female_first';
+    }
+    $name = $this->get_rand_data($first_name_table) . " ";
+
+    $last_name_table = 'humaniti_last';
+    $name .= $this->get_rand_data($last_name_table);
+    return $name;
+  }
+
+  private function get_rand_data(string $table) {
+    try {
+      $dbh = new PDO("sqlite:data/names.db");
+    } catch(PDOException $e) {
+      echo $e->getMessage();
+      exit;
+    }
+    $sql    = ("SELECT * from $table ORDER BY RANDOM() LIMIT 1;");
+    $stmt   = $dbh->query($sql);
+    $result = $stmt->fetch();
+    return  $result['name'];
+  }
+  const STR = 0;
+  const EDU = 4;
+
+  private function go_to_school(array $upp) {
+    $upp[EDU] += 3;
+    return $upp;
+  }
+
+  private function go_to_the_gym(array $upp) {
+    $upp[STR] +=2;
+    return $upp;
+  }
+
+}
 ?>
 
