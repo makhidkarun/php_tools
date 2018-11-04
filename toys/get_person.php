a306a63c6fda950d79d5f711dd1c51d8735b4ef9
Leam Hall <leamhall@gmail.com>
2018-11-04 17:10:30 -0500

    Wrapping up, still works.

--- toys/get_person.php
+++ toys/get_person.php
@@ -3,7 +3,6 @@
 require '../lib/character.php';
 require '../lib/view.php';
 
-#$data_string = file_get_contents('data/soldiers.json');
 $data_string = file_get_contents('data/people_dragons_20180303.json');
 $ds_array = json_decode($data_string, true);
 
