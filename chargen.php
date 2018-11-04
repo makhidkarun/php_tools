bd29d4098635b8c3ff349bb0c632d2ef33ba783c
Leam Hall <leamhall@gmail.com>
2018-11-04 06:23:10 -0500

    Fixed basic object passing in View.

--- chargen.php
+++ chargen.php
@@ -4,7 +4,6 @@ require 'lib/character.php';
 require 'lib/view.php';
 
 $person = new Character();
-#print("$person->name [$person->gender] $person->upp_s\n");
 $this_person = new View($person);
-print("Your new crew: $this_person->to_string .\n");
+print("Your new crew: {$this_person}\n");
 ?>
--- lib/view.php
+++ lib/view.php
@@ -2,9 +2,11 @@
 
 class View {
   public function __construct($person) {
-    $this->person = $person;
+    $this->name   = $person->name;
+    $this->gender = $person->gender;
+    $this->upp_s  = $person->upp_s;
   }
-  public function to_string() {
+  public function __toString() {
     $return_string = "$this->name [$this->gender] $this->upp_s";
     return $return_string;
   }
